6c145877904f4816ad769d81121429f882688a0c
liuxx <liux78@outlook.com>
2019-06-24 22:09:30 +0800

    添加关卡一的场景切换动画

---
+++ Assets/Scripts/LifeCycle/GameEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    [SerializeField]private GameObject mainCamera;
+    public void switchTo(int levelIndex)
+    {
+        mainCamera.GetComponent<SceneFadeInOut>().startFadeOut(delegate() 
+        {
+            SceneManager.LoadScene(levelIndex);
+        });
+        
+    }
+}
---
+++ Assets/Scripts/SceneFadeInOut.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class SceneFadeInOut : MonoBehaviour
+{
+    [SerializeField]
+    public Material material;  //材质球
+    [SerializeField]
+    public float ChangeSpeed = 3f;  
+    private float count;
+    private Action action;
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        StartCoroutine(fadeIn());
+    }
+
+
+    //所有渲染完成后被调用，来渲染图片的后期处理效果 https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnRenderImage.html  
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+
+    IEnumerator fadeIn()
+    {
+        material.SetFloat("_Radius", 0);
+        while (material.GetFloat("_Radius") <= 1.5)
+        {
+            count = material.GetFloat("_Radius") + ChangeSpeed * Time.deltaTime;
+            material.SetFloat("_Radius", count);
+            yield return 0;
+        }
+        yield return 0;
+    }
+
+    IEnumerator fadeOut()
+    {
+        material.SetFloat("_Radius", 1.5f);
+        while (material.GetFloat("_Radius") >= 0)
+        {
+            float change = 0;
+            if(Time.timeScale>0.1f)
+            {
+                change = ChangeSpeed * Time.deltaTime;
+            }
+            else
+            {
+                change = ChangeSpeed * 0.025f;
+            }
+            count = material.GetFloat("_Radius") - change;
+            material.SetFloat("_Radius", count);
+            yield return 0;
+        }
+        action();
+        yield return 0;
+    }
+
+    public void startFadeOut(Action a)
+    {
+        action = a;
+        StartCoroutine(fadeOut());
+    }
+}
+
+
