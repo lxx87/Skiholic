0d37d9800992e4d0ff139fe2f7a50bdd18566557
liuxx <liux78@outlook.com>
2019-07-09 11:42:21 +0800

    一定程度上缓解角色抖动的问题

--- Assets/Scripts/PerpendicularToGround.cs
+++ Assets/Scripts/PerpendicularToGround.cs
@@ -15,20 +15,23 @@ public class PerpendicularToGround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void FixedUpdate()
+    {
         RaycastHit2D hit = Physics2D.Raycast(m_GroundCheck.position, Vector2.down,0.2f,LayerMask.GetMask("Ground"));
         if(hit.collider!=null)
         {
             
             Quaternion nextRot = Quaternion.LookRotation(new Vector3(0,0,1), hit.normal);
-            transform.rotation = Quaternion.Lerp(transform.rotation, nextRot, 0.1f*Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, nextRot, 0.000001f * Time.deltaTime );
             transform.rotation = nextRot;
-
-
         }
         else
         {
             Quaternion nextRot = Quaternion.LookRotation(Vector3.forward,Vector3.up);
-            //transform.rotation = Quaternion.Lerp(transform.rotation, nextRot, 0.1f * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, nextRot, 0.000001f * Time.deltaTime);
             transform.rotation = nextRot;
         }
     }
